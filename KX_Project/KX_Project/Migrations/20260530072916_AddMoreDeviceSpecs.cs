590251c518403c5f42d140b0ab5dc2230885eadd
ArixLautenous <baonamgalaxynote9@gmail.com>
2026-05-30 14:59:46 +0700

    Migrate to EF Core, redesign Product UI, add dynamic device specs

---
+++ KX_Project/KX_Project/Migrations/20260530072916_AddMoreDeviceSpecs.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KX_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoreDeviceSpecs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AudioFormat",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Connectivity",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FrequencyResponse",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GraphicsCard",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Impedance",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Ram",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sensitivity",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Weight",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AudioFormat",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Connectivity",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "FrequencyResponse",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "GraphicsCard",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Impedance",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Ram",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Sensitivity",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Weight",
+                table: "Products");
+        }
+    }
+}
--- KX_Project/KX_Project/Repositories/ICategoryRepository.cs
+++ KX_Project/KX_Project/Repositories/ICategoryRepository.cs
@@ -1,9 +1,16 @@
-﻿using KX_Project.Models;
+using KX_Project.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace KX_Project.Repositories
 {
     public interface ICategoryRepository
     {
         IEnumerable<Category> GetAllCategories();
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category?> GetByIdAsync(int id);
+        Task AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
     }
 }
